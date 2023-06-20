c80aca26f7a109b0eafc6354ef2d3cb42bb81a87
KarlosElyas <karloselyas2@gmail.com>
2023-06-20 17:44:37 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/karlos/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.karlos.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.karlos.cursomc.domain.Cliente;
+import com.karlos.cursomc.repositories.ClienteRepository;
+import com.karlos.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo; // OBJETO de acesso a dados
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		}
+}
