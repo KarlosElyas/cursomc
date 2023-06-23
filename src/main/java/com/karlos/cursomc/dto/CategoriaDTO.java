08dda50a72e9aead87773efcd2721ee593cc5733
KarlosElyas <karloselyas2@gmail.com>
2023-06-23 13:14:06 -0300

    Personalizando o retorno da validação

--- src/main/java/com/karlos/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/karlos/cursomc/dto/CategoriaDTO.java
@@ -11,6 +11,7 @@ import com.karlos.cursomc.domain.Categoria;
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	// A VALIDAÇÃO deve ser feita no FRONT e BACK END, pra evitar enviar inumeras requisições pro backend
 	private Integer id;
 	@NotEmpty(message = "Preenchimento obrigatório")
 	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
--- src/main/java/com/karlos/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/karlos/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,15 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.karlos.cursomc.services.exceptions.DataIntegrityException;
 import com.karlos.cursomc.services.exceptions.ObjectNotFoundException;
 
+// EXCEÇÕES PERSONALIZADAS pro usuario
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+	// exceção personalizada de OBJETO(ID) INFORMADO NÃO EXISTE / NÃO ESTÁ CADASTRADO NO BANCO
 	@ExceptionHandler(ObjectNotFoundException.class) // definido a exceção lançada
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 		
@@ -20,10 +24,23 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
+	// exceção lançada quando tenta deletar um REGISTRO que é referenciado por outros no banco (CASCADE)
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrityFound(DataIntegrityException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	// exceção da validação dos campos
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> dataIntegrityFound(MethodArgumentNotValidException e, HttpServletRequest request){
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage()); // adicionado o nome e message do erro na lista
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 }
