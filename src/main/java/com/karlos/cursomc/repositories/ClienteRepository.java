d96c333db81f0caed82c48999034b9841958316d
KarlosElyas <karloselyas2@gmail.com>
2023-06-27 14:22:59 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/karlos/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/karlos/cursomc/repositories/ClienteRepository.java
@@ -1,9 +1,14 @@
 package com.karlos.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.karlos.cursomc.domain.Cliente;
 
 public interface ClienteRepository extends  JpaRepository<Cliente, Integer>{
 
+	//O Spring Data cria automaticamente o find by com o campo informado | AULA 46
+	@Transactional(readOnly = true) // otimiza o método
+	Cliente findByEmail(String email);
+	
 }
