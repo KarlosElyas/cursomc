c858b849c797da42390f18a3077acd8e06837903
KarlosElyas <karloselyas2@gmail.com>
2023-07-01 18:44:53 -0300

    Enviado o Email em HTML

--- src/main/java/com/karlos/cursomc/services/EmailService.java
+++ src/main/java/com/karlos/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.karlos.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.karlos.cursomc.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
