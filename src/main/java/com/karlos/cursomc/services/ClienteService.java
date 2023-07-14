f423d534d4ad9bfa7506601174d9c404aae5a284
KarlosElyas <karloselyas2@gmail.com>
2023-07-14 14:52:55 -0300

    ajustando tamanho da imagem

--- src/main/java/com/karlos/cursomc/services/ClienteService.java
+++ src/main/java/com/karlos/cursomc/services/ClienteService.java
@@ -51,6 +51,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id) {
 		
 		UserSS user = UserService.authenticated();
@@ -147,6 +150,10 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		// processos para deixar a imagem quadrada e redimensionala todas com um tamanho padrão(PIXEL)
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
--- src/main/java/com/karlos/cursomc/services/ImageService.java
+++ src/main/java/com/karlos/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -40,7 +41,7 @@ public class ImageService {
 	public BufferedImage pngToJpg(BufferedImage img) {
 		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(),
 				BufferedImage.TYPE_INT_RGB);
-		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);// color white prenche os fundos 
+		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);// COLOR WHITE prenche os fundos 
 																				//transparentes de branco
 		return jpgImage;
 	}
@@ -53,5 +54,19 @@ public class ImageService {
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+	
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+			sourceImg, 
+			(sourceImg.getWidth()/2) - (min/2), 
+			(sourceImg.getHeight()/2) - (min/2), 
+			min, 
+			min);		
+	}
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) { // vai redimesionar o tamanho em pixels
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
